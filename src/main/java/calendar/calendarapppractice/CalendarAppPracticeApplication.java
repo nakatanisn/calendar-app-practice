300a94d32852991f5273f79bf5c3bc382e06916f
nakatanis <shun.nakata2@gmail.com>
2018-01-03 05:08:00 +0900

    first commit

---
+++ src/main/java/calendar/calendarapppractice/CalendarAppPracticeApplication.java
@@ -0,0 +1,12 @@
+package calendar.calendarapppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarAppPracticeApplication.class, args);
+	}
+}
---
+++ src/test/java/calendar/calendarapppractice/CalendarAppPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package calendar.calendarapppractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarAppPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
