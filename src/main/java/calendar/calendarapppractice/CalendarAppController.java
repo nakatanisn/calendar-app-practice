f7aacd928d1cccb0710a0319845137fad21888a2
nakatanis <shun.nakata2@gmail.com>
2018-01-14 05:43:21 +0900

    前月/次月/現在月を表示できるように変更

--- src/main/java/calendar/calendarapppractice/CalendarAppController.java
+++ src/main/java/calendar/calendarapppractice/CalendarAppController.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import calendar.calendarapppractice.domain.CalendarApp;
 import calendar.calendarapppractice.service.CalendarAppService;
 import calendar.calendarapppractice.service.CalendarCreate;
 
 @Controller
-@RequestMapping(path = "calendar") // リクエストがマッピングされるURLを定義する
+@RequestMapping("calendar") // リクエストがマッピングされるURLの接頭辞を定義する
 public class CalendarAppController {
 	@Autowired
 	CalendarAppService calendarAppService;
@@ -22,10 +23,9 @@ public class CalendarAppController {
 	@Autowired
 	CalendarCreate calendarCreate;
 	
-	@GetMapping // getScheduleメソッドがURLの/scheduleにマッピングされるように指定
+	@GetMapping
 	// 画面に値を渡すために、Modelオブジェクトを使用
 	String getSchedule(Model model) {
-		
 		// 現在の日付を取得する
 		Calendar cal = Calendar.getInstance(); // カレンダーオブジェクトを生成
 		Integer year = cal.get(Calendar.YEAR); // 現在の年(西暦)を取得
@@ -44,10 +44,67 @@ public class CalendarAppController {
 		model.addAttribute("year", year);
 		model.addAttribute("month", dispMonth);
 		
-		
 		// コントローラのリクエスト処理メソッドの返り値は「ビュー名」すなわち「遷移する画面名」になる
 		// SpringBootではデフォルトで「classpath:template/ + ビュー名 + .html」が画面のパスになる
 		return "calendar/calendar";
 	}
 	
+	// 表示している月の前月を表示する
+	@GetMapping(path = "prevMonth")
+	String prevMonth(@RequestParam Integer curDispYear, @RequestParam Integer curDispMonth, Model model) {
+		Integer year = curDispYear;
+		Integer month = curDispMonth - 1; // 表示している月をjava.util.Calendarでの月の定数に変換
+		month = month - 1; // 表示している月の前月の月の定数を設定
+		// 前年に戻る場合の処理
+		if (month < 0) {
+			month = 11;
+			year = year - 1;
+		}
+		Integer dispMonth = month + 1; // 表示する月は+1する必要あり
+		
+		// findScheduleの結果をModelに設定し、属性名を"schedulesOfTheMonth"として設定
+		List<CalendarApp> schedules = calendarAppService.findSchedule(year, dispMonth);
+		model.addAttribute("schedulesOfTheMonth", schedules);
+		
+		// getCalendarで表示する月のカレンダーを取得
+		List<Integer> dateList = calendarCreate.getCalendar(year, month);
+		model.addAttribute("dateList", dateList);
+		
+		// 現在の年と月も画面に渡す
+		model.addAttribute("year", year);
+		model.addAttribute("month", dispMonth);
+		return "calendar/calendar";
+	}
+	
+	@GetMapping(path = "nextMonth")
+	String nextMonth(@RequestParam Integer curDispYear, @RequestParam Integer curDispMonth, Model model) {
+		Integer year = curDispYear;
+		Integer month = curDispMonth - 1; // 表示している月をjava.util.Calendarでの月の定数に変換
+		month = month + 1; // 表示している月の次月の月の定数を設定
+		// 前年に戻る場合の処理
+		if (month == 12) {
+			month = 0;
+			year = year + 1;
+		}
+		Integer dispMonth = month + 1; // 表示する月は+1する必要あり
+		
+		// findScheduleの結果をModelに設定し、属性名を"schedulesOfTheMonth"として設定
+		List<CalendarApp> schedules = calendarAppService.findSchedule(year, dispMonth);
+		model.addAttribute("schedulesOfTheMonth", schedules);
+		
+		// getCalendarで表示する月のカレンダーを取得
+		List<Integer> dateList = calendarCreate.getCalendar(year, month);
+		model.addAttribute("dateList", dateList);
+		
+		// 現在の年と月も画面に渡す
+		model.addAttribute("year", year);
+		model.addAttribute("month", dispMonth);
+		return "calendar/calendar";
+	}
+	
+	// 現在の月を表示する
+	@GetMapping(params = "goToTop")
+	String goToTop() {
+		return "redirect:/calendar";
+	}
 }
--- src/main/java/calendar/calendarapppractice/service/CalendarCreate.java
+++ src/main/java/calendar/calendarapppractice/service/CalendarCreate.java
@@ -13,13 +13,10 @@ public class CalendarCreate {
 		dispCal.setFirstDayOfWeek(Calendar.MONDAY); // 週の最初の曜日を月曜日に設定（デフォルトでは日曜日始まり）
 		dispCal.set(year, month, 1); // 引数に指定した「年」と「月」を設定
 		
-		Calendar firstDay = Calendar.getInstance(); // 月初めの曜日を取得するためのオブジェクトを生成
-		firstDay.set(Calendar.DATE, 1); // 1日をセット
-		
 		int weeks = dispCal.getActualMaximum(Calendar.WEEK_OF_MONTH); // その月に何週まであるのか、最大週を取得（週の最初が月曜か日曜かで週数が変わる）
 		int days = dispCal.getActualMaximum(Calendar.DAY_OF_MONTH); // その月に何日まであるのか、最大日を取得
 
-		int firstDayOfWeek = firstDay.get(Calendar.DAY_OF_WEEK); // その月のはじめの曜日を取得
+		int firstDayOfWeek = dispCal.get(Calendar.DAY_OF_WEEK); // その月のはじめの曜日を取得
 		
 		// 画面に表示する日数の枠と日付を入れるListを作成
 		List<Integer> list = new ArrayList<>();
@@ -35,12 +32,13 @@ public class CalendarCreate {
 			list.add(i, 0);
 			i++;
 		}
-		
+				
 		int setNum = firstDayOfWeek - 2; // 最初の週で日付を表示しないカレンダーの枠の数を求める 
 		if (setNum == -1) { // 日曜日のみマイナスになるので補正する
 			setNum = 6;
 		} 
 		
+		// listに日付を設定していく
 		int date = 1; // listに設定する日付を初期化
 		while (setNum <= maxNum) { // Listの要素は0から始まるので、setNumがはじめに値を入れる要素番号になる
 			if (date > days) {
@@ -50,7 +48,7 @@ public class CalendarCreate {
 			setNum++;
 			date++;
 		}
-		
+
 		return list;
 	}
 }
